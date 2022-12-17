48595d792acdf7ceafa730cfb19d1ec6b66ec97f
devharp <hsingh.161297@gmail.com>
2022-12-17 12:13:14 +0530

    harpflix-server /treanding api ready

--- mann-stack/harpflix/harpflix-server/src/app.controller.ts
+++ mann-stack/harpflix/harpflix-server/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/trending')
+  getTrending(): Object {
+    return this.appService.getTrending();
+  }
 }
--- mann-stack/harpflix/harpflix-server/src/app.service.ts
+++ mann-stack/harpflix/harpflix-server/src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import * as fetch from 'node-fetch';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getTrending(): Promise<Object>{
+    const link = `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.TMDB_API_KEY}`
+
+    const res = await fetch(link);
+    return await res.json();
   }
 }
